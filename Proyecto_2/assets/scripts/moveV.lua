f9e786d2eeb8a9b561c270adf6bb5145c3cc966f
Jose Mario Castro <josemariocastrochanto@gmail.com>
2025-06-28 00:11:23 -0600

    posiciones de los enemigos

---
+++ Proyecto_2/assets/scripts/moveV.lua
@@ -0,0 +1,18 @@
+state = {}
+
+function update()
+    local id = get_id(this)
+    local t = state[id] or {timer = 0, direction = 1}
+
+    t.timer = t.timer + 1
+
+    if t.timer <= 150 then
+        local x, y = get_position(this)
+        set_position(this, x, y + t.direction)
+    else
+        t.timer = 0
+        t.direction = -t.direction
+    end
+
+    state[id] = t
+end
